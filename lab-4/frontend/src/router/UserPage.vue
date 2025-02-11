b819928415c3d376e41366f2afb5cdaeb03368da
lil-kitten <r.ievlev123@mail.ru>
2025-02-12 01:32:30 +0300

    new validation

--- lab-4/frontend/src/router/UserPage.vue
+++ lab-4/frontend/src/router/UserPage.vue
@@ -1,26 +1,30 @@
 <template>
   <form @submit.prevent="sendPoint">
     <h1>Введите значения</h1>
-    <div>
-      <label for="x">x : </label>
-      <input type="text" id="x" v-model="x" @blur="validateX(x)"/>
+
+    <div v-for="value in values">
+      <label :for="value.key">{{value.key}}:</label>
+      <input
+          type="text"
+          :id="value.key"
+          v-model="value.content"
+          @input="validate(value.content, value.conditions)"
+      />
     </div>
-    <div>
-      <label for="y">y : </label>
-      <input type="text" id="y" v-model="y" @blur="validateY(y)"/>
-    </div>
-    <div>
-      <label for="r">r : </label>
-      <input type="text" id="r" v-model="r" @blur="validateR(r)"/>
-    </div>
+
     <Tools
         :send="sendPoint"
         :clear="clearForm"
         :delete-points="deletePointsInTable"
         :random="randomValues"
-        :is-disabled="!isFormValid"/>
+    />
   </form>
-  <Graph :value-r="r" :points="points" :function="sendClick"/>
+
+  <Graph
+      :value-r="values.find(value => value.key === 'r').content"
+      :points="points" :function="sendClick"
+  />
+
   <Table :points="points"/>
 </template>
 
@@ -32,47 +36,51 @@ import Graph from "@/components/submission/Graph.vue";
 import Table from "@/components/submission/Table.vue";
 
 export default defineComponent({
-  data() {
-    return {
-      points: [],
-      x: '',
-      y: '',
-      r: ''
-    }
-  },
-
-  async mounted() {
-    let newPoints = (await getPoints())
-
-    console.log(newPoints)
-
-    this.points = newPoints
-
-  },
-
   components: {
     Table,
     Graph,
     Tools
   },
 
-  watch: {
-    x(newVal) {
-      this.x = newVal;
-    },
-    y(newVal) {
-      this.y = newVal;
-    },
-    r(newVal) {
-      this.validateR(newVal)
-      this.r = newVal;
+  data() {
+    return {
+      points: [],
+      values: [
+        {
+          key: 'x',
+          content: '',
+          conditions: {
+            min: -3,
+            max: 5,
+            errorText: 'Значение должно быть от -3 до 5'
+          }
+        },
+        {
+          key: 'y',
+          content: '',
+          conditions: {
+            min: -5,
+            max: 5,
+            errorText: 'Значение должно быть от -5 до 5'
+          }
+        },
+        {
+          key: 'r',
+          content: '',
+          conditions: {
+            min: 0,
+            max: 5,
+            errorText: 'Значение должно быть от 0 до 5'
+          }
+        }
+      ]
     }
   },
 
-  computed: {
-    isFormValid() {
-      return this.x.trim() !== '' && this.y.trim() !== '' && this.r.trim() !== '';
-    }
+  async mounted() {
+    let newPoints = (await getPoints())
+    console.log(newPoints)
+    this.points = newPoints
   },
 
   methods: {
@@ -80,60 +88,28 @@ export default defineComponent({
       return parseFloat(value);
     },
 
-    validate(value, {min, max, errorText}) {
-      const num = this.convertToNumber(value);
+    validate(content, conditions) {
+      const num = this.convertToNumber(content);
+      console.log(num)
       if (isNaN(num)) throw new Error('Значение должно быть числом');
-      if (num < min || num > max) throw new Error(errorText);
-    },
-
-    validateX(value) {
-      const trimmed = value.trim();
-      if (trimmed === '') throw new Error('x не может быть пустым');
-      this.validate(trimmed, {
-        min: -3,
-        max: 5,
-        errorText: 'x должен быть между -3 и 5'
-      });
-    },
-
-    validateY(value) {
-      const trimmed = value.trim();
-      if (trimmed === '') throw new Error('y не может быть пустым');
-      this.validate(trimmed, {
-        min: -5,
-        max: 5,
-        errorText: 'y должен быть между -5 и 5'
-      });
-    },
-
-    validateR(value) {
-      const trimmed = value.trim();
-      if (trimmed === '') throw new Error('r не может быть пустым');
-
-      if(trimmed < 0) throw new Error('r не может быть отрицательным и меньше нуля (просто зачем?)')
-
-      this.validate(trimmed, {
-        min: -3,
-        max: 5,
-        errorText: 'r должен быть между -3 и 5'
-      });
+      if (num < conditions.min || num > conditions.max) throw new Error(conditions.errorText);
     },
 
     async sendPoint() {
 
-      this.validateX(this.x);
-      this.validateY(this.y);
-      this.validateR(this.r);
-
-      const point = JSON.stringify({
-        x: parseFloat(this.x.trim()),
-        y: parseFloat(this.y.trim()),
-        r: parseFloat(this.r.trim())
+      this.values.forEach(value => {
+        this.validate(value.content, value.conditions);
       });
+
+      const point = JSON.stringify(
+          this.values.reduce((acc, value) => {
+            acc[value.key] = parseFloat(value.content.trim());
+            return acc;
+          }, {})
+      );
 
       try {
         const response = await addPoint(point);
-
 
         console.log('nice job!');
         console.log(response);
@@ -146,24 +122,15 @@ export default defineComponent({
     },
 
     async sendClick(point) {
-      try {
-        const response = await addPoint(point);
 
+        this.validate(point.r, this.values.find(value => value.key === 'r').conditions)
+
+        const response = await addPoint(point);
 
         console.log('nice job!');
         console.log(response);
 
         this.points = (await getPoints()).data
-      } catch (error) {
-        console.error('Error:', error.message);
-        throw error;
-      }
-    },
-
-    clearForm() {
-      this.x = '';
-      this.y = '';
-      this.r = '';
     },
 
     async deletePointsInTable() {
@@ -171,17 +138,20 @@ export default defineComponent({
       await deletePoints();
     },
 
-    randomValues() {
-      const minX = -3,
-          maxX = 5,
-          minY = -5,
-          maxY = 5,
-          minR = -3,
-          maxR = 5;
+    clearForm() {
+      this.values.forEach(value => {
+        value.content = ''
+      })
+    },
 
-      this.r = (Math.random() * (maxR - minR) + minR).toFixed(2);
-      this.x = (Math.random() * (maxX - minX) + minX).toFixed(2);
-      this.y = (Math.random() * (maxY - minY) + minY).toFixed(2);
+
+    randomValues() {
+      this.values.forEach(value => {
+        const randomValue = (Math.random() * (value.conditions.max - value.conditions.min) + value.conditions.min)
+            .toFixed(2)
+        console.log(randomValue)
+        value.content = randomValue
+      });
     },
 
   }
