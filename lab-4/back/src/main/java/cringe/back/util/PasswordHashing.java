4b7b0ec0460ec807be152674b1b85f001bdfde0f
lil-kitten <r.ievlev123@mail.ru>
2025-02-16 10:25:09 +0300

    =)

--- lab-4/back/src/main/java/cringe/back/util/PasswordHashing.java
+++ lab-4/back/src/main/java/cringe/back/util/PasswordHashing.java
@@ -3,8 +3,10 @@ package cringe.back.util;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordHashing {
+    private static final String SALT = BCrypt.gensalt();
+
     public String hash(String password) {
-        return BCrypt.hashpw(password, "SALT");
+        return BCrypt.hashpw(password, SALT);
     }
 
     public boolean verify(String password, String hashedPassword) {
